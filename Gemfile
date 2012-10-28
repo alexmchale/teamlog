7162383792738863d2525a20f1b93f5a02ae3c57
Alex McHale <alex@anticlever.com>
2012-10-28 07:04:45 -0500

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+gem 'pg'
+
+gem 'bcrypt-ruby'
+gem 'haml'
+gem 'jquery-rails'
+gem 'simple_form'
+gem 'strong_parameters'
+
+group :assets do
+  gem 'coffee-rails', '~> 3.2.2'
+  gem 'compass-rails', '~> 1.0.3'
+  gem 'sass-rails', '~> 3.2.5'
+  gem 'uglifier', '~> 1.3.0'
+end
+
+group :development, :test do
+  gem 'awesome_print', require: 'ap'
+  gem 'factory_girl_rails'
+  gem 'minitest'
+  gem 'shoulda'
+  gem 'turn'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsSkeleton::Application.config.secret_token = '73b86c14778edea7e35ea7349aaa9c179c45a0021db089948aec3ace6b353c720fd4d047f13aa17c52a4b49d179105bee0b644572db36af3a868177719e41007'
