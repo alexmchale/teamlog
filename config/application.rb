79cd99adbdb45bafbbb5ca1312a0672072d7bc20
Alex McHale <alex@anticlever.com>
2013-07-21 21:26:50 -0500

    Some tweaks for heroku.

--- config/application.rb
+++ config/application.rb
@@ -65,6 +65,9 @@ module RailsSkeleton
     config.autoload_paths << Rails.root.join("app", "decorators")
     config.autoload_paths << Rails.root.join("app", "presenters")
 
+    # Don't start up Rails when precompiling.
+    config.assets.initialize_on_precompile = false
+
     # Configure sendgrid.
     config.action_mailer.smtp_settings = {
       :user_name            => ENV["SENDGRID_USERNAME"],
